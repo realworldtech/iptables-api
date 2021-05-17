04fce0c40a1f31910e697b087097904d189017a0
Karl Kloppenborg <k@rl.ag>
2021-05-17 09:41:13 +0000

    bugfix: port and install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mv iptables-api /usr/local/bin/iptables-api
-mv iptables-api.service /lib/systemd/user/iptables-api.service
-chmod 755 /lib/systemd/user/iptables-api.service
+mv iptables-api.service /lib/systemd/system/iptables-api.service
+chmod 755 /lib/systemd/system/iptables-api.service
